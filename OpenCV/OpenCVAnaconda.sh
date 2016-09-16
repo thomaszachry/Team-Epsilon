a791325fa449023b46ad185264c67cc33416f03f
randythiencao <randythiencao@yahoo.com>
2016-09-16 16:39:39 -0500

    add OpenCV script for Anaconda Python 3.5

---
+++ OpenCV/OpenCVAnaconda.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#This script will install OpenCV and OpenCV3 for use with ANACONDA PYTHON 3.5
+conda -y install -c https://conda.binstar.org/menpo opencv
+conda -y install -c https://conda.binstar.org/menpo opencv3
+pip -y install imutils
