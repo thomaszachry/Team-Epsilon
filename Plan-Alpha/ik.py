e203a7ceeb226074de4c2d0ef3de78fb72d33997
Randy Thien Cao <randythiencao@yahoo.com>
2016-11-13 19:13:33 -0600

    add IK python code

---
+++ Plan-Alpha/ik.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import math
+
+RADIANS = 0
+DEGREES = 1
+
+l1 = 12.5 #Length of link 1
+l2 = 13.5 #length of link 2
+
+
+
+#IK for just the 2 links
+def invkin2(x, y, angleMode=DEGREES):
+    """Returns the angles of the first two links
+    in the robotic arm as a list.
+    returns -> (th1, th2)
+    input:
+    x - The x coordinate of the effector
+    y - The y coordinate of the effector
+    angleMode - tells the function to give the angle in
+                degrees/radians. Default is degrees
+    output:
+    th1 - angle of the first link w.r.t ground
+    th2 - angle of the second link w.r.t the first link"""
+
+    #stuff for calculating th2
+    r_2 = x**2 + y**2
+    l_sq = l1**2 + l2**2
+    term2 = (r_2 - l_sq)/(2*l1*l2)
+    term1 = ((1 - term2**2)**0.5)*-1
+    #calculate th2
+    th2 = math.atan2(term1, term2)
+    #optional line.
+    th2 = -1*th2
+
+    #Stuff for calculating th2
+    k1 = l1 + l2*math.cos(th2)
+    k2 = l2*math.sin(th2)
+    r  = (k1**2 + k2**2)**0.5
+    gamma = math.atan2(k2,k1)
+    #calculate th1
+    th1 = math.atan2(y,x) - gamma
+
+    if(angleMode == RADIANS):
+        return th1, th2
+    else:
+        return math.degrees(th1), math.degrees(th2)
+
+
+if __name__ == "__main__":
+    print invkin2(0, 200, DEGREES)
