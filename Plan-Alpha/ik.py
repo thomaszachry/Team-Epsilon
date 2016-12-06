dc53dc42abdc9b83a203753ea58f180cee89c08d
randythiencao <randythiencao@yahoo.com>
2016-12-06 15:23:56 -0600

    error checking

--- Plan-Alpha/ik.py
+++ Plan-Alpha/ik.py
@@ -24,26 +24,28 @@ def invkin2(x,y):
     th2 - angle of the second link w.r.t the first link"""
 
 
-	
-    #stuff for calculating th2
-    r_2 = x**2 + y**2
-    l_sq = l1**2 + l2**2
-    term2 = (r_2 - l_sq)/(2*l1*l2)
-    term1 = ((1 - term2**2)**0.5)*-1
-    #calculate th2
-    th2 = math.atan2(term1, term2)
-    #optional line.
-    th2 = -1*th2
+    try:	
+        #stuff for calculating th2
+        r_2 = x**2 + y**2
+        l_sq = l1**2 + l2**2
+        term2 = (r_2 - l_sq)/(2*l1*l2)
+        term1 = ((1 - term2**2)**0.5)*-1
+        #calculate th2
+        th2 = math.atan2(term1, term2)
+        #optional line.
+        th2 = -1*th2
 
-    #Stuff for calculating th2
-    k1 = l1 + l2*math.cos(th2)
-    k2 = l2*math.sin(th2)
-    r  = (k1**2 + k2**2)**0.5
-    gamma = math.atan2(k2,k1)
-    #calculate th1
-    th1 = math.atan2(y,x) - gamma
-
-    return math.degrees(th1), math.degrees(th2)
+        #Stuff for calculating th2
+        k1 = l1 + l2*math.cos(th2)
+        k2 = l2*math.sin(th2)
+        r  = (k1**2 + k2**2)**0.5
+        gamma = math.atan2(k2,k1)
+        #calculate th1
+        th1 = math.atan2(y,x) - gamma
+    except ValueError:
+        return -999,-999
+    else:
+        return math.degrees(th1), math.degrees(th2)
 
 
 if __name__ == "__main__":
